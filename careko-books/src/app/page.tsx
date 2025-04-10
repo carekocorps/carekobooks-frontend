c7d1ac2c055356cf99b3af2f1fc897201daecad1
Isaac Duarte <isaacunhaid@gmail.com>
2025-04-10 17:30:47 -0300

    feat - melhorias e componentização do carrossel de livros

--- careko-books/src/app/page.tsx
+++ careko-books/src/app/page.tsx
@@ -2,56 +2,67 @@ import Banner from "@/components/program/banners";
 import Book from "@/components/program/book";
 import Activity from "@/components/program/activity";
 import { Separator } from "@/components/ui/separator";
+import CarouselBooks from "@/components/program/books-carousel";
 
 export default function Home() {
   return (
-    <section className="flex flex-col w-350 y- mt-4 gap-15">
-          <Banner
-          image1="/distopia.png"
-          image2="/ad.png"
+      <section className="flex flex-col w-350 y- mt-4 gap-15">
+      <Banner image1="/distopia.png" image2="/ad.png" />
+
+      <div className="flex gap-40">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl text-[#2E2E2E] flex items-center gap-4">
+            <i className="bi bi-lightbulb-fill"></i>
+            Popular na sua rede
+          </h1>
+          <Separator orientation="horizontal" className="h-px bg-gray-300" />
+
+          <div className="w-218">
+            <CarouselBooks
+              books={[
+                <Book key="1" image="/metamorfose.png" name="A Metamorfose" />,
+                <Book key="2" image="/image.png" name="Biriba" />,
+                <Book key="3" image="/larissa.png" name="O Diário de Larissa Manoela" />,
+                <Book key="4" image="/rezende.png" name="Dois Mundos, Um Herói" />,
+                <Book key="5" image="/assim.png" name="É Assim que Acaba" />,
+                <Book key="6" image="/sapiens.png" name="Sapiens" />,
+              ]}
+            />
+          </div>
+
+          <h1 className="text-2xl text-[#2b1919] flex items-center gap-4">
+            <i className="bi bi-book-fill"></i>
+            Continue Lendo
+          </h1>
+          <Separator orientation="horizontal" className="h-px bg-gray-300" />
+
+          <div className="w-218">
+          <CarouselBooks
+            books={[
+              <Book key="11" image="/larissa.png" name="O Diário de Larissa Manoela" />,
+            ]}
           />
+          </div>
 
-          <div className="flex gap-40">
-            <div className="flex flex-col gap-4">
-              <h1 className="text-2xl text-[#2E2E2E] flex items-center gap-4">
-                <i className="bi bi-lightbulb-fill"></i>
-                Popular na sua rede
-                </h1>
-              <Separator orientation="horizontal" className="h-px bg-gray-300"/>
-            
-              <div className="flex gap-6 justify-start w-full mb-5">
-                <Book image="/metamorfose.png" name="A Metamorfose"/>
-                <Book image="/image.png" name="Biriba"/>
-                <Book image="/larissa.png" name="O Diário de Larissa Manoela"/>
-                <Book image="/rezende.png" name="Dois Mundos, Um Herói"/>
-              </div>
+          <h1 className="text-2xl text-[#2E2E2E] flex items-center gap-4">
+            <i className="bi bi-bookmarks-fill"></i>
+            Livros mais recentes
+          </h1>
+          <Separator orientation="horizontal" className="h-px bg-gray-300" />
 
-              <h1 className="text-2xl text-[#2b1919] flex items-center gap-4">
-                <i className="bi bi-book-fill"></i>
-                Continue Lendo
-                </h1>
-              <Separator orientation="horizontal" className="h-px bg-gray-300"/>
-       
+          <div className="w-218">
+          <CarouselBooks
+            books={[
+              <Book key="7" image="/metamorfose.png" name="A Metamorfose" />,
+              <Book key="8" image="/image.png" name="Biriba" />,
+              <Book key="9" image="/larissa.png" name="O Diário de Larissa Manoela" />,
+              <Book key="10" image="/rezende.png" name="Dois Mundos, Um Herói" />,
+            ]}
+          />
+          </div>
+        </div>
 
-                <div className="flex gap-6 justify-start w-full mb-5">
-                  <Book image="/larissa.png" name="O Diário de Larissa Manoela"/>
-                </div>
-
-              <h1 className="text-2xl text-[#2E2E2E] flex items-center gap-4">
-                <i className="bi bi-bookmarks-fill"></i>
-                Livros mais recentes
-                </h1>
-                <Separator orientation="horizontal" className="h-px bg-gray-300"/>
-
-                <div className="flex gap-6 justify-start w-full ">
-                  <Book image="/metamorfose.png" name="A Metamorfose"/>
-                  <Book image="/image.png" name="Biriba"/>
-                  <Book image="/larissa.png" name="O Diário de Larissa Manoela"/>
-                  <Book image="/rezende.png" name="Dois Mundos, Um Herói"/>
-                </div>
-            </div>
-
-            <div className="flex flex-col gap-4 border-3 rounded-xl">
+        <div className="flex flex-col gap-4 border-3 rounded-xl">
             <h1 className="text-2xl text-white bg-[#0f2027] p-3 rounded-xl flex items-center justify-between">
               Atividade
               <i className="bi-people-fill text-white text-xl" />
@@ -65,7 +76,7 @@ export default function Home() {
                   <Activity username="@Amanda" livro="É assim que acaba" horario="12:00"/>
               </div>
             </div>
-          </div>
-          </section>
+      </div>
+    </section>
   );
 }
--- careko-books/src/components/program/activity.tsx
+++ careko-books/src/components/program/activity.tsx
@@ -9,7 +9,7 @@ type ActivityProps = {
 
 export default function Activity({username, livro, horario}: ActivityProps){
     return(
-        <div className="w-full bg-white rounded-xl flex flex-col shadow-md p-13">
+        <div className="w-full bg-white rounded-xl flex flex-col shadow-md p-10">
             <h2 className="text-[#2E2E2E] text-xl">hoje, às {horario}</h2>
             <h2 className="text-[#2E2E2E] text-xl">
                 <strong>{username}</strong> adicionou o livro {livro} na lista de favoritos
